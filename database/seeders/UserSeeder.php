0017c41b2ba773b03dfb144c14805bde3d362452
frknbrky1 <fb0990@hotmail.com>
2022-01-10 17:51:03 +0300

    Quiz migration create, Factory and Seed create

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,33 +1,27 @@
 <?php
 
 namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-
-        $this->call([
-            UserSeeder::class,
-            QuizSeeder::class,
-        ]);
-        
-        /*\App\Models\User::insert([
+        \App\Models\User::insert([
             'name' => 'Furkan Berkay',
             'email' => 'fb0990@hotmail.com',
             'email_verified_at' => now(),
             'type' => 'admin',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => \Illuminate\Support\Str::random(10)
-        ]);*/
-
-        // \App\Models\User::factory(5)->create();
-        // \App\Models\Quiz::factory(10)->create();
+        ]);
+        
+        \App\Models\User::factory(5)->create();
     }
 }
