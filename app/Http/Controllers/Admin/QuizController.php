a2950cedc6b9f8839182a1e946feed59fe0574b8
frknbrky1 <fb0990@hotmail.com>
2022-01-11 23:47:33 +0300

    #7

--- app/Http/Controllers/Admin/QuizController.php
+++ app/Http/Controllers/Admin/QuizController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Quiz;
+use App\Http\Requests\QuizCreateRequest;
 
 class QuizController extends Controller
 {
@@ -36,9 +37,11 @@ class QuizController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(QuizCreateRequest $request)
     {
-        return $request->post();
+        Quiz::create($request->post());
+        return redirect()->route('quizzes.index')->withSuccess('Quiz Başarıyla oluşturuldu');
+        //return $request->post();
     }
 
     /**
--- resources/views/admin/quiz/create.blade.php
+++ resources/views/admin/quiz/create.blade.php
@@ -6,21 +6,21 @@
                 @csrf
                 <div class="mb-3">
                     <label class="form-label">Quiz Başlığı*</label>
-                    <input type="text" name="title" class="form-control" required >
+                    <input type="text" name="title" class="form-control" value="{{old('title')}}" >
                 </div>
                 <div class="mb-3">
                     <label class="form-label">Quiz Açıklama</label>
-                    <textarea name="description" class="form-control" rows="4"></textarea>
+                    <textarea name="description" class="form-control" rows="4">{{old('description')}}</textarea>
                 </div>
                 <div class="mb-3 form-check">
-                    <input class="form-check-input" type="checkbox" id="isFinished">
+                    <input class="form-check-input" @if(old('finished_at')) checked @endif type="checkbox" id="isFinished">
                     <label class="form-check-label" for="isFinished">
                         Bitiş Tarihi olacak mı ?
                     </label>
                 </div>
-                <div class="mb-3" id="finishedInput" style="display: none">
+                <div class="mb-3" id="finishedInput" @if(!old('finished_at')) style="display: none" @endif>
                     <label class="form-label">Bitiş Tarihi</label>
-                    <input type="datetime-local" name="finished_at" class="form-control" >
+                    <input type="datetime-local" name="finished_at" id="reqDate" class="form-control" value="{{old('finished_at')}}">
                 </div>
                 <div class="mb-3">
                     <button type="submit" class="btn btn-success btn-sm w-100">Quiz Oluştur</button>
@@ -33,9 +33,11 @@
             $('#isFinished').change(function () {
                 if($(this).is(':checked')) {
                     $('#finishedInput').show();
+                    $("#reqDate").prop('required',true);
                 }
                 else {
                     $('#finishedInput').hide();
+                    $("#reqDate").prop('required',false);
                 }
             });
         </script>
